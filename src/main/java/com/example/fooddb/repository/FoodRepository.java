7085ab119ca060eab1a8ce8560a8970a8006c345
Kavisara Samarakoon <wmkksamarakoon@students.nsbm.ac.lk>
2026-01-15 12:31:24 +0530

    deleting older files

--- src/main/java/com/example/fooddb/repository/FoodRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.fooddb.repository;
-
-import com.example.fooddb.model.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food, Long> {}
